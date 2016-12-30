10ce377a7f05a66c9bc05d8c322f2be5cad8d2e5
yuokada <callistoiv+git@gmail.com>
2016-12-30 18:34:05 +0900

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,20 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
 username = 'yokada'
-home_dir = format('/home/%s/', username)
+home_dir = format('/home/%s', username)
+
 # https://docs.chef.io/resource_user.html
 user username do
   gid 100
   password '$6$4WFoWETj$Stn/jzddz.D7Xtt.IlXnlLHgxu.cgVKqajMbc3qUYQasfMHE9BxuVnaGcKssBdz0CLGsVd9KuZ56ltsbTyd2h.'
-  home '/home/yokada'
+  home home_dir
   shell '/bin/zsh'
   action :create
 end
 # yuokada ALL=(ALL)       NOPASSWD: ALL
-directory home_dir + '.ssh' do
+directory home_dir + '/.ssh' do
   owner username
   group 'users'
   mode 00755
@@ -22,7 +24,7 @@ directory home_dir + '.ssh' do
   action :create
 end
 
-template home_dir + '.ssh/authorized_keys' do
+template home_dir + '/.ssh/authorized_keys' do
   source 'authorized_keys.erb'
   owner username
   group 'users'
@@ -33,7 +35,7 @@ end
 # hive user
 user 'hive' do
   supports manage_home: true
-  action :create
+  action :remove
 end
 
 include_recipe 'conoha::packages'
